1c20de84ba5e760361a6cdb3c7086e0000bc8c80
linyufeng <leolin49@foxmail.com>
2021-10-13 16:24:18 +0800

    update

---
+++ 02.04. Partition List LCCI/test.hpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#include "../include/helper.hpp"
+#include "../include/singly-linked-list.h"
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* small = new ListNode(0);
+        ListNode* large = new ListNode(0);
+        ListNode* smallHead = small;
+        ListNode* largeHead = large;
+        while (head) {
+            if (head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
